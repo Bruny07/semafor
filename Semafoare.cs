aa86929948964daf8fbe9ea3ffbf150d717c11e0
Bruny07 <balint.bruno94@gmail.com>
2016-05-16 22:29:31 +0300

    Adaugat toate intersectile + thread-uri pt fiecare

--- Semafoare.cs
+++ Semafoare.cs
@@ -5,18 +5,19 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace pipestream
+namespace semafor
 {
     class Intersectii
     {
         public void Intersecti()
         {
-            Thread interCosm = new Thread(new ThreadStart(IntersectieCosminului.intersectie));
-            Thread interSalc = new Thread(new ThreadStart(IntersectieSalcamilor.intersectie));
-            interCosm.IsBackground = false;
-            interSalc.IsBackground = false;
-            interCosm.Start();
-            interSalc.Start();
+            Thread interCosm = new Thread(new ThreadStart(IntersectieCosminului.intersectie)); interCosm.IsBackground = true; interCosm.Start();
+            Thread interSalc = new Thread(new ThreadStart(IntersectieSalcamilor.intersectie)); interSalc.IsBackground = true; interSalc.Start();
+            Thread interDrub = new Thread(new ThreadStart(IntersectieDrubeta.intersectie)); interDrub.IsBackground = true; interDrub.Start();
+            Thread interTrai = new Thread(new ThreadStart(IntersectieTraiana.intersectie)); interTrai.IsBackground = true; interTrai.Start();
+            Thread interBran = new Thread(new ThreadStart(IntersectieBrancoveanu.intersectie)); interBran.IsBackground = true; interBran.Start();
+            Thread interHebe = new Thread(new ThreadStart(IntersectieHebe.intersectie)); interHebe.IsBackground = true; interHebe.Start();
+            Thread interRane = new Thread(new ThreadStart(IntersectieRanetii.intersectie)); interRane.IsBackground = true; interRane.Start();
         }
     }
     
@@ -155,13 +156,36 @@ namespace pipestream
             }
         }
     }
+    class IntersectieRanetii
+    {
+        public static int semInainte = 0;
+        public static void intersectie()
+        {
+            while (true)
+            {
+                if (semInainte == 0)
+                {
+                    semInainte = 1;
+                    Console.WriteLine("Semafor Intersectie Cosminului: Verde");
+                    Thread.Sleep(5000);
+                }
+                else if (semInainte == 1)
+                {
+                    semInainte = 0;
+                    Console.WriteLine("Semafor Intersectie Cosminului: Rosu");
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+    }
+
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            int contor = 0;
+           // int contor = 0;
             Intersectii p = new Intersectii();
             //Parallel.Invoke(p.Intersecti);
             p.Intersecti();
